9c76ff75044475935206d2c2bfd67694d490941d
nsdeschenes <nsdeschenes@outlook.com>
2025-12-02 06:36:41 -0400

    complete day 1

---
+++ day-1/index.ts
@@ -0,0 +1,93 @@
+async function solvePartOne(filename: string): Promise<void> {
+  const input = await Bun.file(filename).text();
+  const lines = input.split("\n");
+
+  let currentClick = 50;
+  let zeroHitCounter = 0;
+
+  for (const line of lines) {
+    const direction = line[0];
+    const clickCount = parseInt(line.slice(1));
+
+    if (direction === "L") {
+      for (let i = 0; i < clickCount; i++) {
+        if (currentClick === 0) {
+          currentClick = 99;
+          continue;
+        }
+
+        currentClick--;
+      }
+    }
+
+    if (direction === "R") {
+      for (let i = 0; i < clickCount; i++) {
+        if (currentClick === 99) {
+          currentClick = 0;
+          continue;
+        }
+
+        currentClick++;
+      }
+    }
+
+    if (currentClick === 0) {
+      zeroHitCounter++;
+    }
+  }
+
+  console.log("zeroHitCounter", zeroHitCounter);
+}
+
+async function solvePartTwo(filename: string): Promise<void> {
+  const input = await Bun.file(filename).text();
+  const lines = input.split("\n");
+
+  let currentClick = 50;
+  let zeroHitCounter = 0;
+
+  let counter = 0;
+  for (const line of lines) {
+    const direction = line[0];
+    const clickCount = parseInt(line.slice(1));
+
+    if (direction === "L") {
+      for (let i = 0; i < clickCount; i++) {
+        if (currentClick === 0 && i !== 0) {
+          zeroHitCounter++;
+        }
+
+        if (currentClick === 0) {
+          currentClick = 99;
+          continue;
+        }
+
+        currentClick--;
+      }
+    }
+
+    if (direction === "R") {
+      for (let i = 0; i < clickCount; i++) {
+        if (currentClick === 0 && i !== 0) {
+          zeroHitCounter++;
+        }
+
+        if (currentClick === 99) {
+          currentClick = 0;
+          continue;
+        }
+
+        currentClick++;
+      }
+    }
+
+    if (currentClick === 0) {
+      zeroHitCounter++;
+    }
+  }
+
+  console.log("zeroHitCounter", zeroHitCounter);
+}
+
+await solvePartOne("puzzle-input");
+await solvePartTwo("puzzle-input");
